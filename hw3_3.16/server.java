150bc56243ca9ecfd7508b39103a06210ecadfba
benjihuang <benjihuang0422@gmail.com>
2019-03-18 19:13:19 +0800

    新增說明  改random

--- hw3_3.16/server.java
+++ hw3_3.16/server.java
@@ -1,9 +1,16 @@
 import java.net.*;
 import java.io.*;
+import java.util.Random;
 public class server
 {
 	public static void main(String[] args) {
 		try {
+			String[] word = {
+			"Good Job!",
+			"Thank you!",
+			"Are you alright?",
+			"Nice Dude!",
+			"Nice work!"}; 
 			ServerSocket sock = new ServerSocket(6013);
 			/* now listen for connections */
 			while (true) {
@@ -11,7 +18,9 @@ public class server
 				PrintWriter pout = new
 				PrintWriter(client.getOutputStream(), true);
 				/* write the Date to the socket */
-				pout.println("good job!");
+				Random r=new Random();
+        		int randomN=r.nextInt(word.length);
+				pout.println(word[randomN]);
 				/* close the socket and resume */
 				/* listening for connections */
 				client.close();
